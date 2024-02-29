bdf9dd4b5c560e5e000d1667acae80a8b2785e9a
Anna Spjut <anna.spjut@gmail.com>
2024-03-01 00:48:15 +0400

    [add] mail + dadi

--- README.md
+++ README.md
@@ -6,9 +6,26 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 
+## Svoglimento
+
+1. const creare lista mail utenti
+2. promt per chiedere mail utente
+3. verifica se nella lista mail utenti permessi
+4. creare messaggio da stampare in pagina per l'utente
+
 #### Gioco dei dadi
 
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 ## Svoglimento
+
+1. MathRandom 2 volte per estrarre numUtente e numComputer
+2. if per controllo quale num è più alto
+3. messaggio chi è il vincitore
+
+ 
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,53 @@
+//Mail////
+
+// const allowedEmails = [
+//   'anna.spjut@gmail.com',
+//   'pinko.pallino@gmail.com',
+//   'mario.rossi@gmail.com',
+//   'giuseppe.verdi@gmail.com',
+//   'maria@gmail.com',
+//   'boolean@gmail.com',
+// ]
+
+// const userEmail = prompt('Inserisci la tua mail');
+
+// for(let i = 0; i < allowedEmails.length; i++){
+//   const email = allowedEmails[i];
+//   if(email === userEmail){
+//     document.querySelector('h1').innerHTML = `Sei un utente iscritto, hai accesso!`
+//   }
+//   else{
+//     document.querySelector('h1').innerHTML = `Mi dispiace non sei iscritto, non hai accesso!`
+//   }
+// };
+
+
+
+//Dadi////
+
+const numUtente = Math.floor(Math.random() *6 + 1);
+const numComputer = Math.floor(Math.random() *6 + 1);
+console.log(numComputer, numUtente)
+
+if (numUtente < numComputer){
+  risultato = 'Ha vinto il computer!'
+}else if (numUtente > numComputer){
+  risultato = 'Hai vinto tu!'
+}else{
+  risultato = 'Pareggio, gioca di nuovo!'
+};
+
+output.innerHTML = risultato;
+
+
+
+
+
+
+
+
+
+
+
+
+  
